29ae5591679b7eabf50e4df4cdbdb7ebaef44798
Evinne8 <pe41bnd@eduvaud.ch>
2025-09-24 10:15:48 +0200

    code(ReadFile): lire le fichier Gpx à l'aide d'Aspose.Gis et stocker les points dans la liste trackpoints
    [45MIN][DONE]

--- personnel/rando/RandoBase/Rando/Rando/Rando.cs
+++ personnel/rando/RandoBase/Rando/Rando/Rando.cs
@@ -1,7 +1,13 @@
+﻿using Aspose.Gis;
+using Aspose.Gis.Geometries;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
 namespace Rando
 {
     public partial class Rando : Form
     {
+        private List<Trackpoint> _trackpoints = new List<Trackpoint>();
         public Rando()
         {
             InitializeComponent();
@@ -12,8 +18,49 @@ namespace Rando
             Pen myPen = new Pen(Color.Red);
             myPen.Width = 2;
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            ReadGpx("C:\\Users\\pe41bnd\\Documents\\GitHub\\323-Programmation_fonctionnelle\\personnel\\rando\\gpx\\Ballade_châtaignère_🌰.gpx");
+            
+            System.Drawing.Point[] points = new System.Drawing.Point[4] { new System.Drawing.Point(30, 50), new System.Drawing.Point(50, 10), new System.Drawing.Point(80, 50), new System.Drawing.Point(111, 400) };
             this.CreateGraphics().DrawLines(myPen, points);
         }
+
+
+        public void ReadGpx(string filePath)
+        {
+            // This code example demonstrates how to read waypoints from GPX file
+            // Load the GPX file
+            var layer = Drivers.Gpx.OpenLayer(filePath);
+            Debug.WriteLine("path : " + filePath);
+            layer.ToList().ForEach(feature =>
+            {
+                Debug.WriteLine("douce : " + feature.Geometry.GeometryType);
+                // Check for Point geometry
+                if (feature.Geometry.GeometryType == GeometryType.MultiLineString)
+                {
+                    Debug.WriteLine("dedant : " + feature.ToString());
+                    // Read Points
+                    var lines = (MultiLineString)feature.Geometry;
+                    lines.ToList().ForEach(line =>
+                    {
+                        List<string> coordoneeslines = line.AsText().ToString().Replace("LINESTRING Z (", "").Replace(")", "").Split(",").ToList();
+                        coordoneeslines.ForEach(coordoneesline => {
+                            
+                            List<string> coords = coordoneesline.ToString().Trim().Split(" ").ToList();
+
+
+                            double lag = double.Parse(coords.First().ToString());
+                            double lon = double.Parse(coords.Skip(1).First().ToString());
+                            double ele = double.Parse(coords.Last().ToString());
+                            Debug.WriteLine("lag : " + lag);
+                            Debug.WriteLine("lon : " + lon);
+                            Debug.WriteLine("ele : " + ele);
+                            _trackpoints.Add(new Trackpoint(lag,lon,ele));
+                        });
+
+                    });
+                }
+            });
+        }
+
     }
 }
---
+++ personnel/rando/RandoBase/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rando
+{
+    class Trackpoint
+    {
+        private double _latitude;
+        private double _longitude;
+        private double _elevation;
+
+        public Trackpoint(double latitude, double longitude, double elevation)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+            _elevation = elevation;
+        }
+    }
+}
