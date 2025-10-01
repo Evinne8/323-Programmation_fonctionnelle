b572a422f302ef30c5caaec996659d4dbaab35bc
Evan Sottile <pe41bnd@eduvaud.ch>
2025-10-01 11:05:11 +0200

    code(todo8): todo8 terminé

--- personnel/diffit/diffit/Program.cs
+++ personnel/diffit/diffit/Program.cs
@@ -110,6 +110,9 @@ comparisons.ForEach(c =>
 });
 
 // TODO: 08 Afficher le nombre de lignes identiques et différentes entre les 2 fichiers
+Console.WriteLine($"Il y a {comparisons.Count - diffLines.Count} lignes identiques entre les deux fichiers !");
+Console.WriteLine($"Il y a {diffLines.Count} lignes différentes entre les deux fichiers !");
+
 
 // TODO: 09 Définir une fonction qui compte les différences (caractères différents) entre deux textes (sera utilisé pour les 2 lignes de A et B...)
 // Pour info/rappel, la fonction Zip (comme une fermeture éclair) permet d’associer deux listes.
